b462bb0a859cb3fe5c57057d07685611bb987e34
Gokhan Kus <gkus1998@hotmail.com>
2024-10-21 13:48:05 +0300

    Add project files.

---
+++ RabbitMQ.Publisher/Program.cs
@@ -0,0 +1,33 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+namespace RabbitMQ.Publisher
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			var factory = new ConnectionFactory();
+			string amqpUrl = "amqps://opmzghqv:Gcm-XamhxS2oH2oNCdgYhmApsU1oUeol@rat.rmq2.cloudamqp.com/opmzghqv";
+			factory.Uri = new Uri(amqpUrl);
+
+			using var connection = factory.CreateConnection();
+
+			var channel = connection.CreateModel();
+			string queueName = "hello-queue";
+
+			//durable kuyrugun kalıcı olup olmadıgını belirler true olursa kuyruk kalıcı,
+			//exclusive kuyrugun sadece o connection tarafından kullanılabilir oldugunu belirtir false yaparak RabbitMQ.Subscriber tarafında kullanacagiz
+			//autoDelete kuyrugun otomatik olarak silinip silinmeyecegini belirtir false yaparak kuyruklar consumerlara (subscriber) ulassa bile silinmez 
+			//cunku bazen mesaj dogru islenmeeyebilir o yuzden mesaj hemen silinmesin, ozetle daha dayanıklı bir yapı icin true, false, false seklinde yazılır
+			channel.QueueDeclare(queue:queueName, durable: true, exclusive: false, autoDelete: false);
+
+			string message = "first rabbitmq message";
+			var messageBody = Encoding.UTF8.GetBytes(message);
+			channel.BasicPublish(string.Empty, queueName, null, messageBody);
+
+			Console.WriteLine("message has been sent");
+			Console.ReadLine();
+		}
+	}
+}
---
+++ RabbitMQ.Subscriber/Program.cs
@@ -0,0 +1,35 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+namespace RabbitMQ.Subscriber
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			var factory = new ConnectionFactory();
+			string amqpUrl = "amqps://opmzghqv:Gcm-XamhxS2oH2oNCdgYhmApsU1oUeol@rat.rmq2.cloudamqp.com/opmzghqv";
+			factory.Uri = new Uri(amqpUrl);
+
+			using var connection = factory.CreateConnection();
+
+			var channel = connection.CreateModel();
+			string queueName = "hello-queue";
+
+			//bu satiri publisher'da yazdigimiz icin burada tekrar tanimlamamiza gerek yok, eger orada tanimlamasaydik queue yoksa burada da olusturabilirdi.
+			//channel.QueueDeclare(queueName, true, false, false);
+
+			//consumer = subscriber 
+			var consumer = new EventingBasicConsumer(channel);
+			channel.BasicConsume(queueName, true, consumer);
+
+			consumer.Received += (object? sender, BasicDeliverEventArgs e) =>
+			{
+				var message = Encoding.UTF8.GetString(e.Body.ToArray());
+				Console.WriteLine("Gelen mesaj: " + message);
+			};
+			Console.ReadLine();
+		}
+	}
+}
