73b70e2f49ec83bf6ea01e67a82714cecaf66186
Gokhan Kus <gkus1998@hotmail.com>
2024-10-23 19:01:36 +0300

    mvc project ve product controller, product.cs, AppDbContext.cs ve ilgili paketler..

---
+++ RabbitMQWeb.Watermark/Models/Product.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RabbitMQWeb.Watermark.Models
+{
+	public class Product
+	{
+		public Product(int id, int stock, string name, decimal price, string imageUrl)
+		{
+			Id = id;
+			Stock = stock;
+			Name = name;
+			Price = price;
+			ImageUrl = imageUrl;
+		}
+		public Product()
+		{
+
+		}
+		[Key]
+		public int Id { get; set; }
+
+		[Range(0,100)]
+		public int Stock { get; set; }
+
+		[StringLength(100)]
+		public string? Name { get; set; }
+
+		[Column(TypeName = "decimal(18,2)")]
+		public decimal Price { get; set; }
+
+		[StringLength(100)]
+        public string? ImageUrl{ get; set; }
+    }
+}
---
+++ RabbitMQWeb.Watermark/Program.cs
@@ -0,0 +1,40 @@
+using Microsoft.EntityFrameworkCore;
+using RabbitMQWeb.Watermark.Models;
+
+namespace RabbitMQWeb.Watermark
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			var builder = WebApplication.CreateBuilder(args);
+
+			// Add services to the container.
+
+			builder.Services.AddDbContext<AppDbContext>(options =>
+			{
+				options.UseInMemoryDatabase(databaseName: "productDb");
+			});
+			builder.Services.AddControllersWithViews();
+
+			var app = builder.Build();
+
+			// Configure the HTTP request pipeline.
+			if (!app.Environment.IsDevelopment())
+			{
+				app.UseExceptionHandler("/Home/Error");
+			}
+			app.UseStaticFiles();
+
+			app.UseRouting();
+
+			app.UseAuthorization();
+
+			app.MapControllerRoute(
+				name: "default",
+				pattern: "{controller=Home}/{action=Index}/{id?}");
+
+			app.Run();
+		}
+	}
+}
